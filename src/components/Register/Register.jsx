434a3a08a3e8e5c4d273cf0235bea570c72dcd12
lamia <lamianisha3@gmai..com>
2024-10-21 01:34:23 +0600

    login page created

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -7,10 +7,11 @@ import { GiCrossedSwords } from "react-icons/gi";
 
 
 const Register = () => {
+
+
     const [errorMsg, setErrorMsg] = useState('');
     const [successMsg, setSuccessMsg] = useState('');
     const [show, setShow] = useState(true);
-
 
     // majhe majhe auto import hoina jodi vs code onek khon por open kora hoi .... tokhon jekono kicu export korle auto refresh hoe jabe
     const handleRegister = e => {
@@ -51,7 +52,6 @@ const Register = () => {
     };
 
     const handleShowPass = () => {
-        console.log('show');
         setShow(!show);
     };
 
@@ -59,13 +59,13 @@ const Register = () => {
 
     return (
         <div>
-            <h1 className="text-3xl my-10 font-bold">Please Register</h1>
+            <h1 className="text-3xl my-10 font-bold">Register</h1>
             <form onSubmit={handleRegister} className="space-y-5 relative w-1/2 mx-auto ">
                 <input placeholder="Name" className="input input-bordered w-full input-success" type="text" name="name" />
-                <input placeholder="Email" className="input input-bordered w-full input-success" type="email" name="email" />
+                <input placeholder="Email" className="input input-bordered w-full input-success" required type="email" name="email" />
                 <div className="relative">
-                    <input placeholder="Password" className="input input-bordered w-full input-success" type={show ? "password" : "text"} name="password" />
-                    <button className="text-xl absolute top-3 right-6" onClick={handleShowPass}>{show ? <GiEnergySword></GiEnergySword> : <GiCrossedSwords></GiCrossedSwords>}</button>
+                    <input placeholder="Password" className="input input-bordered w-full input-success" required type={show ? "password" : "text"} name="password" />
+                    <span className="text-xl absolute top-4 right-6" onClick={handleShowPass}>{show ? <GiEnergySword></GiEnergySword> : <GiCrossedSwords></GiCrossedSwords>}</span>
                 </div>
                 {errorMsg && <h1 className="text-red-500">{errorMsg}</h1>}
                 {successMsg && <p className="text-green-400">{successMsg}</p>}
@@ -74,8 +74,6 @@ const Register = () => {
                 <br />
                 <input className="btn btn-outline" type="submit" value="submit" />
             </form>
-
-
         </div>
     );
 };
