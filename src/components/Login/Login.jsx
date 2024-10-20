e94929737d590d9014379f2964688f17318768cf
lamia <lamianisha3@gmai..com>
2024-10-21 01:46:05 +0600

    test2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -35,7 +35,17 @@ const Login = () => {
     return (
         <div>
             <h1 className="text-3xl my-10 font-bold">Login</h1>
-
+            <form onSubmit={handleLogin} className="space-y-5 relative w-1/2 mx-auto ">
+                <input placeholder="Email" className="input input-bordered w-full input-success" required type="email" name="email" />
+                <div className="relative">
+                    <input placeholder="Password" className="input input-bordered w-full input-success" required type={show ? "password" : "text"} name="password" />
+                    <span className="text-xl absolute top-4 right-6" onClick={handleShowPass}>{show ? <GiEnergySword></GiEnergySword> : <GiCrossedSwords></GiCrossedSwords>}</span>
+                </div>
+                {errorMsg && <h1 className="text-red-500">{errorMsg}</h1>}
+                {successMsg && <p className="text-green-400">{successMsg}</p>}
+                <br />
+                <input className="btn btn-outline" type="submit" value="submit" />
+            </form>
         </div>
     );
 };
