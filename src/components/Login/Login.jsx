8fd1ffc31f86cbe9feec8b11d0c40dde486d5ff8
Kaiser <ricoreven@gmail.com>
2024-10-21 16:16:55 +0600

    implemented reset email for user

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef, useState } from "react";
 import { GiEnergySword } from "react-icons/gi";
 import { GiCrossedSwords } from "react-icons/gi";
 import auth from "../../firebase/firebase.config";
@@ -10,6 +10,8 @@ const Login = () => {
     const [errorMsg, setErrorMsg] = useState('');
     const [successMsg, setSuccessMsg] = useState('');
     const [show, setShow] = useState(true);
+
+    const emailRef = useRef(null);
 
     const handleLogin = e => {
         e.preventDefault();
@@ -31,18 +33,44 @@ const Login = () => {
         setShow(!show);
     };
 
+    const handleForgetPass = () => {
+        const email = emailRef.current.value;
+        if (!email) {
+            setErrorMsg('Provide The Email !');
+            return;
+        }
+        else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+            setErrorMsg('provide a valid email');
+            return;
+        }
+
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                setSuccessMsg('Reset email sent successfully');
+            })
+            .catch(error => {
+                setErrorMsg(error.message);
+            });
+
+
+
+    };
+
+
 
     return (
         <div>
             <h1 className="text-3xl my-10 font-bold">Login</h1>
             <form onSubmit={handleLogin} className="space-y-5 relative w-1/2 mx-auto ">
-                <input placeholder="Email" className="input input-bordered w-full input-success" required type="email" name="email" />
+                <input placeholder="Email" ref={emailRef} className="input input-bordered w-full input-success" required type="email" name="email" />
                 <div className="relative">
                     <input placeholder="Password" className="input input-bordered w-full input-success" required type={show ? "password" : "text"} name="password" />
                     <span className="text-xl absolute top-4 right-6" onClick={handleShowPass}>{show ? <GiEnergySword></GiEnergySword> : <GiCrossedSwords></GiCrossedSwords>}</span>
                 </div>
                 {errorMsg && <h1 className="text-red-500">{errorMsg}</h1>}
                 {successMsg && <p className="text-green-400">{successMsg}</p>}
+                <br />
+                <span className="hover:underline cursor-pointer" onClick={handleForgetPass}>Forget Password</span>
                 <br />
                 <input className="btn btn-outline" type="submit" value="submit" />
             </form>
